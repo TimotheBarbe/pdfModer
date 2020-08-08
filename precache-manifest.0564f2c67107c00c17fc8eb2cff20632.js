90f6e2972ec954ce197cb49993097c768b242688
tbarbe <timothe.barbe@gmail.com>
2020-08-08 18:30:52 +0200

    Updates

---
+++ precache-manifest.0564f2c67107c00c17fc8eb2cff20632.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0aadaa68f4b0ac7a47f0",
+    "url": "/pdfModer/static/js/runtime~main.b854f828.js"
+  },
+  {
+    "revision": "468bc4813fff82a3c424",
+    "url": "/pdfModer/static/js/main.99aa2902.chunk.js"
+  },
+  {
+    "revision": "e8ed4c58a785aa02e07e",
+    "url": "/pdfModer/static/js/2.77b7bd81.chunk.js"
+  },
+  {
+    "revision": "468bc4813fff82a3c424",
+    "url": "/pdfModer/static/css/main.8d4345ac.chunk.css"
+  },
+  {
+    "revision": "4912a25498ba406327fa6fbf6d99c916",
+    "url": "/pdfModer/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pdfModer/precache-manifest.ca4500a1cacba0c01f7717dd3fea63c8.js"
+  "/pdfModer/precache-manifest.0564f2c67107c00c17fc8eb2cff20632.js"
 );
 
 workbox.clientsClaim();
